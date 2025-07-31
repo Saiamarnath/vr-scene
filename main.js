06336dfac9009e5523639bc6baaaab15b060277e
Sai Amarnath G <guntnursaiamarnath@gmail.com>
2025-07-31 22:40:34 +0530

    fix

--- main.js
+++ main.js
@@ -138,7 +138,7 @@ function render() {
         moveDirection.normalize();
 
         // ✅ FIX: adjust gaze direction for rig’s 270° spawn rotation
-        moveDirection.applyAxisAngle(new THREE.Vector3(0, 1, 0), -rig.rotation.y);
+        moveDirection.applyAxisAngle(new THREE.Vector3(0, 0, 1), -rig.rotation.y);
         // Move target
         moveTarget.copy(rig.position).add(moveDirection.clone().multiplyScalar(MOVE_DISTANCE));
         isMoving = true;
